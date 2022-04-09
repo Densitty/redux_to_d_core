933e14ef81337f6791cee33f7f1fdacff797dac2
Densitty <dennis.brough@yandex.com>
2022-04-09 09:23:54 -0500

    using currying to create composite fxns

--- src/index.js
+++ src/index.js
@@ -2,29 +2,27 @@ import { compose, pipe } from "lodash/fp";
 
 let input = "  I love writing JS   ";
 
-// output can be achieved in a functional programming way
-// first trim
 const trimStr = (str) => str.trim();
-// we can convert the str in input to lowercases
 const convertToLower = (str) => str.toLowerCase();
-// wrap a string in div - which does not know whether str is trimmed or not
-const wrapInDiv = (str) => `<div>${str}</div>`;
+// const wrapInSpan = (str) => `<span>${str}</span>`;
+// const wrapInDiv = (str) => `<div>${str}</div>`;
 
-// to create a divElement that wraps a trimmed string, we can compose the 2 fxns
-const res = wrapInDiv(convertToLower(trimStr(input)));
-
-// res has 2 problems; we have to read the expression 4rm right to left to understand what is going to be returned and then also many parentheses; imagine having many functions
-
-// we can solve the above problem using lodash and the utility functions inside it
-const transform = compose(wrapInDiv, convertToLower, trimStr);
-const res2 = transform(input);
+// fxns wrapInSpan & wrapInDiv are similar, hence we can write
+const wrapInElement = (elem, str) => `<${elem}>${str}</${elem}>`;
+const wrapInElement2 = (elem) => (str) => `<${elem}>${str}</${elem}>`;
 
 // we can use the pipe function to further simplify things by writing the functions in the order we want to apply it instead of doing it from right to left as we have in transform
-const transform2 = pipe(trimStr, convertToLower, wrapInDiv);
-const res3 = transform2(input);
+const transform = pipe(trimStr, convertToLower, wrapInElement);
+const res = transform(input);
+// which will give the meaningless result below
+// <i love writing js>undefined</i love writing js>
+// because we have introduced 2 args instead of one and pipe reads the fxn from left to right, passing only 1 arg to wrapInElement instead of 2 expected.
+// To solve this problem, we use currying
 
-console.log({
-  res,
-  res2,
-  res3,
-});
+const transform2 = pipe(trimStr, convertToLower, wrapInElement2("div"));
+const res2 = transform2(input);
+
+const transform3 = pipe(trimStr, convertToLower, wrapInElement2("p"));
+const res3 = transform3(input);
+
+console.log({ res, res2, res3 });
