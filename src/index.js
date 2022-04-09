c7adb0c59a30e4d8cebfdcb518d8d3a037810670
Densitty <dennis.brough@yandex.com>
2022-04-09 11:14:57 -0500

    mutating an array in an immutable way

--- src/index.js
+++ src/index.js
@@ -1,65 +1,19 @@
-// pure functions cant change or mutate args; hence we have a concept called immutability (i.e once an object is created, it cannot be changed but if we want to change it, we have to create a copy of it and then change that copy)
+const numbers = [1, 2, 3];
 
-let myName = "john";
-let nameUpper = myName.toUpperCase(); // myName is unaffected
-
-// in JS, objects and arrays are not immutable unlike string;
-const person = { name: "John" };
-person.name = "Tim"; // which is against rules of functional programming
-
-// to mutate an object in FP, we first copy the object
-const girl = { name: "Alice" };
-
-// we create a copy using either of the b=methods below
-const girlMutateUsingObjectAssign = Object.assign({}, girl, {
-  name: "Alice Wang",
-  married: false,
-});
-
-const girlMutateUsingSpread = {
-  ...girl,
-  name: "Alice C. Wang",
-  age: 21,
-  married: false,
-};
-
-console.log({ girl, girlMutateUsingObjectAssign, girlMutateUsingSpread });
-
-// however both Object.Assign & {...spread} shallow copies
-const friend = {
-  name: "Andy",
-  age: 22,
-  location: { city: "Prague", country: "Czech Republic" },
-};
-
-const friendUpdate = { ...friend };
-friendUpdate.name = "Andy Wong";
-// because of shallow copy, friend.location.city too will be updated/mutated (violation of FP)
-friendUpdate.location.city = "Brno";
-
-// console.log(friend.location.city); // Brno
-// console.log(friendUpdate.location.city); // Brno
-console.log({ friend, friendUpdate });
-
-// to deeply copy an object
-const girlFriend = {
-  name: "Loveth",
-  location: { city: "Lagos", country: "Nigeria" },
-};
-
-const girlFriendMutated = {
-  ...girlFriend,
-  location: {
-    ...girlFriend.location,
-  },
-};
-
-girlFriendMutated.location.city = "Abuja";
+// mutating an array in an immutable way
+// Adding
+const addNewNum = [4, ...numbers];
+// insert an item to a particular index in an array (e.g insert at index 2)
+const insertNum = [...numbers.slice(0, 2), 5, ...numbers.slice(2)];
+// removing an item (say 3)
+const removeNum = numbers.filter((n) => n !== 3);
+// updating an item (if we are dealing with an array of objects, spread the object element i.e instead of 20, we do {...} )
+const updateNum = numbers.map((num) => (num === 2 ? 20 : num));
 
 console.log({
-  girlFriend,
-  girlFriendMutated,
+  numbers,
+  addNewNum,
+  insertNum,
+  removeNum,
+  updateNum,
 });
-
-// deep copying object refers to objects that have nested objects like we have in friend and girlFriend objects
-// deep copying objects is very verbose (imagine having more nested objects) but we can use a library to help out of verbosity
