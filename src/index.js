c046690eca19ccdfc344f05c1afdbf2988e59037
Densitty <dennis.brough@yandex.com>
2022-04-09 08:38:25 -0500

    using lodash to simplify our composition function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-// an ideal functional programming is to write some small & reusable fxns and compose them to build more complex fxns for solving real world problems
+import { compose, pipe } from "lodash/fp";
 
 let input = "  I love writing JS   ";
-let output = `<div>${input.trim()}</div>`;
-// output creates a div element & hosts our trimmed input in it
-console.log(output);
 
 // output can be achieved in a functional programming way
 // first trim
@@ -11,9 +8,23 @@ const trimStr = (str) => str.trim();
 // we can convert the str in input to lowercases
 const convertToLower = (str) => str.toLowerCase();
 // wrap a string in div - which does not know whether str is trimmed or not
-const wrapDiv = (str) => `<div>${str}</div>`;
+const wrapInDiv = (str) => `<div>${str}</div>`;
 
 // to create a divElement that wraps a trimmed string, we can compose the 2 fxns
-const res = wrapDiv(convertToLower(trimStr(input)));
+const res = wrapInDiv(convertToLower(trimStr(input)));
 
 // res has 2 problems; we have to read the expression 4rm right to left to understand what is going to be returned and then also many parentheses; imagine having many functions
+
+// we can solve the above problem using lodash and the utility functions inside it
+const transform = compose(wrapInDiv, convertToLower, trimStr);
+const res2 = transform(input);
+
+// we can use the pipe function to further simplify things by writing the functions in the order we want to apply it instead of doing it from right to left as we have in transform
+const transform2 = pipe(trimStr, convertToLower, wrapInDiv);
+const res3 = transform2(input);
+
+console.log({
+  res,
+  res2,
+  res3,
+});
