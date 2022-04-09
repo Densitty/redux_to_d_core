e0614f94c5eff77d3f0d0eac613e8039abbf5ea6
Densitty <dennis.brough@yandex.com>
2022-04-09 12:08:46 -0500

    enforcing immutability using Immer.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
-import { Map } from "immutable";
-// JS is not a pure functional program but we can use libraries that can help enforce immutability in the language using libraries like Immutable, Immer or Mori
+import { produce } from "immer";
+// Immer simplifies the data structure better than Immutable does
 
-const book1 = { title: "The Boy with the Longest Name" };
+const book = { title: "The Boy with the Longest Name" };
 
-// the fxn publishBook is mutating object directly; violation of functional programming
+// Immer makes it easy to work with our object as if we were doing the spread operator or Object.Assign() unlike what we have in Immutable.js
 function publishBook(book) {
-  book.isPublished = true;
+  return produce(book, (draftBook) => {
+    draftBook.isPublished = true;
+  });
 }
 
-publishBook(book1);
+const mutatedBook = publishBook(book);
 
-// using Immutable.js
-const book2 = Map({ title: "Alice in Wonderland" });
-// console.log(book2); // a different DS from what we expect {title: "..."}
-// using Immutable.js, we cant access properties directly like
-// console.log(book2.title); // undefined
-// to access ppties
-console.log(book2.get("title"));
-// to get the object we are expecting
-console.log(book2.toJS());
-// to mutate book2 in an immutable way using Immutable.js
-function publishStatus(book) {
-  return book.set("isPublished", true);
-}
-
-const book2Mutated = publishStatus(book2);
-console.log(book2Mutated.toJS());
+console.log({
+  book,
+  mutatedBook,
+});
