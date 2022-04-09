6d3d1b9e2680eaeea7d8f7adfa46bdbe5be83ea8
Densitty <dennis.brough@yandex.com>
2022-04-09 11:36:48 -0500

    enforcing immutability using Immutable.js library

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-const numbers = [1, 2, 3];
+import { Map } from "immutable";
+// JS is not a pure functional program but we can use libraries that can help enforce immutability in the language using libraries like Immutable, Immer or Mori
 
-// mutating an array in an immutable way
-// Adding
-const addNewNum = [4, ...numbers];
-// insert an item to a particular index in an array (e.g insert at index 2)
-const insertNum = [...numbers.slice(0, 2), 5, ...numbers.slice(2)];
-// removing an item (say 3)
-const removeNum = numbers.filter((n) => n !== 3);
-// updating an item (if we are dealing with an array of objects, spread the object element i.e instead of 20, we do {...} )
-const updateNum = numbers.map((num) => (num === 2 ? 20 : num));
+const book1 = { title: "The Boy with the Longest Name" };
 
-console.log({
-  numbers,
-  addNewNum,
-  insertNum,
-  removeNum,
-  updateNum,
-});
+// the fxn publishBook is mutating object directly; violation of functional programming
+function publishBook(book) {
+  book.isPublished = true;
+}
+
+publishBook(book1);
+
+// using Immutable.js
+const book2 = Map({ title: "Alice in Wonderland" });
+// console.log(book2); // a different DS from what we expect {title: "..."}
+// using Immutable.js, we cant access properties directly like
+// console.log(book2.title); // undefined
+// to access ppties
+console.log(book2.get("title"));
+// to get the object we are expecting
+console.log(book2.toJS());
+// to mutate book2 in an immutable way using Immutable.js
+function publishStatus(book) {
+  return book.set("isPublished", true);
+}
+
+const book2Mutated = publishStatus(book2);
+console.log(book2Mutated.toJS());
