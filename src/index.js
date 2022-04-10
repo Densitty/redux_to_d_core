20c0f71a9205cd22b8a26f6bc6bfd0107022bd7b
Densitty <dennis.brough@yandex.com>
2022-04-10 01:40:28 -0500

    the form of redux code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import { produce } from "immer";
-// Immer simplifies the data structure better than Immutable does
+import store from "./store";
+import { bugAdded, bugRemoved, bugResolved } from "./actionCreators";
 
-const book = { title: "The Boy with the Longest Name" };
-
-// Immer makes it easy to work with our object as if we were doing the spread operator or Object.Assign() unlike what we have in Immutable.js
-function publishBook(book) {
-  return produce(book, (draftBook) => {
-    draftBook.isPublished = true;
+// store.subscribe() is what gives update on the ui concerning what is happening; we need to store it so that if we navigate to another page, like if the remove bug is on another page, the "add bug" will be removed/not logged
+const unsubscribe = store.subscribe(() => {
+  console.log({
+    status: "Store Changed",
+    state: store.getState(),
   });
-}
-
-const mutatedBook = publishBook(book);
-
-console.log({
-  book,
-  mutatedBook,
 });
+// supposing we click on the "Add Bug" button on the UI, the corresponding action has to be dispatched (called)
+store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugAdded("Bug 2"));
+
+store.dispatch(bugResolved(1));
+// to see what is inside our store
+console.log(store.getState());
+// liken the unsubscribe as "remove event handler"
+unsubscribe();
+// supposing the remove button is clicked on, then the corresponding action also need to be dispatched
+store.dispatch(bugRemoved());
---
+++ src/reducer.js
@@ -0,0 +1,37 @@
+import * as actions from "./actionTypes";
+
+// initialize an id
+let lastId = 0;
+
+function reducer(state = [], action) {
+  switch (action.type) {
+    case actions.BUG_ADDED:
+      return [
+        ...state, // 1st copy all d bugs in state []/[{...},{...}]
+        {
+          // add a new one
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+
+    case actions.BUG_REMOVED:
+      // just remove d bug with d id present in payload
+      return state.filter((bug) => bug.id !== action.payload.id);
+
+    case actions.BUG_RESOLVED:
+      return state.map((bug) => {
+        // console.log(bug); //{id: 1, description: "", resolved: false}
+        if (bug.id !== action.payload.id) {
+          return bug;
+        }
+        return { ...bug, resolved: true };
+      });
+
+    default:
+      return state;
+  }
+}
+
+export default reducer;
