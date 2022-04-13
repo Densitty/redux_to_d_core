f78f23e175b58fc2524218c10948f4f68653c309
Densitty <dennis.brough@yandex.com>
2022-04-13 06:15:32 -0500

    combining different reducers together in a redux application

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import reducer from "./bugs";
+/* separate reducers */
+// import reducer from "./bugs";
 // import reducer from "./projects";
+/* ***************** */
+// combined reducers which conbine reducers for both bugs and projects
+import reducer from "./reducer";
 
 export default function configureAppStore() {
   const store = configureStore({
