47404d67fdc1778e580581873d7e66c14a305ba8
Densitty <dennis.brough@yandex.com>
2022-04-21 10:19:29 -0500

    adding an example of mutating an array of objects immutably

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const numbers = [1, 2, 3];
+const friends = [
+  { name: "John", age: 19 },
+  { name: "Sally", age: 17 },
+  { name: "Allen", age: 18 },
+];
 
 // mutating an array in an immutable way
 // Adding
@@ -9,6 +14,10 @@ const insertNum = [...numbers.slice(0, 2), 5, ...numbers.slice(2)];
 const removeNum = numbers.filter((n) => n !== 3);
 // updating an item (if we are dealing with an array of objects, spread the object element i.e instead of 20, we do {...} )
 const updateNum = numbers.map((num) => (num === 2 ? 20 : num));
+// mutate an array of object immutably
+const updatedFriends = friends.map((friend) => {
+  return friend.name === "John" ? { ...friend, name: "Jonny" } : friend;
+});
 
 console.log({
   numbers,
@@ -16,4 +25,6 @@ console.log({
   insertNum,
   removeNum,
   updateNum,
+  updatedFriends,
+  // friends,
 });
