03446e4bc8bc88c36a266b06b2ccb80d212da612
Densitty <dennis.brough@yandex.com>
2022-04-21 09:52:13 -0500

    redux in an api consumption

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,11 +1,9 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import func from "./middleware/func";
+
 import logger2 from "./middleware/logger";
-import logger from "./middleware/logger";
 import toast from "./middleware/toast";
+import api from "./middleware/api";
 /* separate reducers */
-// import reducer from "./bugs";
-// import reducer from "./projects";
 /* ***************** */
 // combined reducers which conbine reducers for both bugs and projects
 import reducer from "./reducer";
@@ -14,9 +12,7 @@ export default function configureAppStore() {
   const store = configureStore({
     reducer,
     // func is our own written middleware function
-    // middleware: [logger2("console"), func],
-    // redux toolkit has function middlewares made available for us
-    middleware: [...getDefaultMiddleware(), logger2("console"), toast],
+    middleware: [...getDefaultMiddleware(), logger2("console"), toast, api],
   });
   return store;
 }
